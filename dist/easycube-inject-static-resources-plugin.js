2c9ac8183f00b3edcb122c49280cc28141cb64cb
右究 <aisowe@sina.com>
2023-10-18 18:23:27 +0800

    feat: update version

--- dist/easycube-inject-static-resources-plugin.js
+++ dist/easycube-inject-static-resources-plugin.js
@@ -1,70 +1,46 @@
-function n(i) {
-  const t = `
-      <link as="style" rel="/static/easycube-playground@0.1.4.min.css"/>
-      <link as="script" rel="/static/dayjs@1.11.10.min.js"/>
-      <link as="script" rel="/static/dayjs@1.11.10-plugin-advancedFormat.js"/>
-      <link as="script" rel="/static/dayjs@1.11.10-plugin-customParseFormat.js"/>
-      <link as="script" rel="/static/dayjs@1.11.10-plugin-localeData.js"/>
-      <link as="script" rel="/static/dayjs@1.11.10-plugin-quarterOfYear.js"/>
-      <link as="script" rel="/static/dayjs@1.11.10-plugin-weekday.js"/>
-      <link as="script" rel="/static/dayjs@1.11.10-plugin-weekOfYear.js"/>
-      <link as="script" rel="/static/dayjs@1.11.10-plugin-weekYear.js"/>
-      <link as="script" rel="/static/dayjs@1.11.10-locale-zh-cn.js"/>
-      <link as="script" rel="/static/lodash@4.17.21.min.js"/>
-      <link as="script" rel="/static/echarts@5.4.3.min.js"/>
-      <link as="script" rel="/static/axios@0.27.2.min.js"/>
-      <link as="script" rel="/static/easycube-request@0.1.1.iife.min.js"/>
-      <link as="script" rel="/static/popper.js@2.11.8.min.js"/>
-      <link as="script" rel="/static/tippy.js@6.3.7.umd.min.js"/>
-      <link as="script" rel="/static/vue@3.3.4.runtime.global.prod.min.js"/>
-      <link as="script" rel="/static/ant-design-vue@3.2.20-with-locales.min.js"/>
-      <link as="script" rel="/static/request@0.1.0.iife.min.js"/>
-      <link as="script" rel="/static/vue-demi@0.14.6.iife.min.js"/>
-      <link as="script" rel="/static/vueuse_shared@10.4.1.iife.min.js"/>
-      <link as="script" rel="/static/vueuse_core@10.4.1.iife.min.js"/>
-      <link as="script" rel="/static/vue-router@4.1.5.global.min.js"/>
-      <link as="script" rel="/static/pinia@2.1.6.iife.min.js"/>
-      <link as="script" rel="/static/sortable@1.15.0.min.js"/>
-      <link as="script" rel="/static/vuedraggable.umd.min.js"/>
-      <link as="script" rel="/static/easycube-playground@0.1.4.iife.min.js"/>
-
-      <link rel="stylesheet" href="/static/easycube-playground@0.1.4.min.css" />
-      <script src="/static/dayjs@1.11.10.min.js" defer><\/script>
-      <script src="/static/dayjs@1.11.10-plugin-advancedFormat.js" defer><\/script>
-      <script src="/static/dayjs@1.11.10-plugin-customParseFormat.js" defer><\/script>
-      <script src="/static/dayjs@1.11.10-plugin-localeData.js" defer><\/script>
-      <script src="/static/dayjs@1.11.10-plugin-quarterOfYear.js" defer><\/script>
-      <script src="/static/dayjs@1.11.10-plugin-weekday.js" defer><\/script>
-      <script src="/static/dayjs@1.11.10-plugin-weekOfYear.js" defer><\/script>
-      <script src="/static/dayjs@1.11.10-plugin-weekYear.js" defer><\/script>
-      <script src="/static/dayjs@1.11.10-locale-zh-cn.js" defer><\/script>
-      <script src="/static/lodash@4.17.21.min.js" defer><\/script>
-      <script src="/static/echarts@5.4.3.min.js" defer><\/script>
-      <script src="/static/axios@0.27.2.min.js" defer><\/script>
-      <script src="/static/easycube-request@0.1.1.iife.min.js" defer><\/script>
-      <script src="/static/popper.js@2.11.8.min.js" defer><\/script>
-      <script src="/static/tippy.js@6.3.7.umd.min.js" defer><\/script>
-      <script src="/static/vue@3.3.4.runtime.global.prod.min.js" defer><\/script>
-      <script src="/static/ant-design-vue@3.2.20-with-locales.min.js" defer><\/script>
-      <script src="/static/request@0.1.0.iife.min.js" defer><\/script>
-      <script src="/static/vue-demi@0.14.6.iife.min.js" defer><\/script>
-      <script src="/static/vueuse_shared@10.4.1.iife.min.js" defer><\/script>
-      <script src="/static/vueuse_core@10.4.1.iife.min.js" defer><\/script>
-      <script src="/static/vue-router@4.1.5.global.min.js" defer><\/script>
-      <script src="/static/pinia@2.1.6.iife.min.js" defer><\/script>
-      <script src="/static/sortable@1.15.0.min.js" defer><\/script>
-      <script src="/static/vuedraggable.umd.min.js" defer><\/script>
-      <script src="/static/easycube-playground@0.1.4.iife.min.js" defer><\/script>
-    `;
+const t = [
+  { name: "easycube-playground@0.1.4.min.css", type: "style" },
+  { name: "dayjs@1.11.10.min.js", type: "script" },
+  { name: "dayjs@1.11.10-plugin-advancedFormat.js", type: "script" },
+  { name: "dayjs@1.11.10-plugin-customParseFormat.js", type: "script" },
+  { name: "dayjs@1.11.10-plugin-localeData.js", type: "script" },
+  { name: "dayjs@1.11.10-plugin-quarterOfYear.js", type: "script" },
+  { name: "dayjs@1.11.10-plugin-weekday.js", type: "script" },
+  { name: "dayjs@1.11.10-plugin-weekOfYear.js", type: "script" },
+  { name: "dayjs@1.11.10-plugin-weekYear.js", type: "script" },
+  { name: "dayjs@1.11.10-locale-zh-cn.js", type: "script" },
+  { name: "lodash@4.17.21.min.js", type: "script" },
+  { name: "echarts@5.4.3.min.js", type: "script" },
+  { name: "axios@0.27.2.min.js", type: "script" },
+  { name: "easycube-request@0.1.1.iife.min.js", type: "script" },
+  { name: "popper.js@2.11.8.min.js", type: "script" },
+  { name: "tippy.js@6.3.7.umd.min.js", type: "script" },
+  { name: "vue@3.3.4.runtime.global.prod.min.js", type: "script" },
+  { name: "ant-design-vue@3.2.20-with-locales.min.js", type: "script" },
+  { name: "request@0.1.0.iife.min.js", type: "script" },
+  { name: "vue-demi@0.14.6.iife.min.js", type: "script" },
+  { name: "vueuse_shared@10.4.1.iife.min.js", type: "script" },
+  { name: "vueuse_core@10.4.1.iife.min.js", type: "script" },
+  { name: "vue-router@4.1.5.global.min.js", type: "script" },
+  { name: "pinia@2.1.6.iife.min.js", type: "script" },
+  { name: "sortable@1.15.0.min.js", type: "script" },
+  { name: "vuedraggable.umd.min.js", type: "script" },
+  { name: "easycube-playground@0.1.4.iife.min.js", type: "script" }
+], n = t.map((e) => `<link as="${e.type}" ref="preload" href="/static/${e.name}">`).join(""), i = t.map((e) => e.type === "style" ? `<link rel="stylesheet" href="/static/${e.name}" />` : `<script src="/static/${e.name}" preload><\/script>`).join(""), r = `
+  ${n}
+  ${i}
+`;
+function y(e) {
   return {
     name: "inject-static-resources-plugin",
     transformIndexHtml(s) {
-      return i ? s.replace(/<head>([\s\S]*?)<\/head>/g, (a, r) => `<head>
-${t}
-${r}</head>`) : s;
+      return e ? s.replace(/<head>([\s\S]*?)<\/head>/g, (m, a) => `<head>
+              ${r}
+              ${a}
+            </head>`) : s;
     }
   };
 }
 export {
-  n as default
+  y as default
 };
--- index.ts
+++ index.ts
@@ -1,69 +1,64 @@
 import type { Plugin } from 'vite'
 
-export default function injectStaticResources(isBuild: boolean): Plugin {
-  const resources = `
-      <link as="style" rel="/static/easycube-playground@0.1.4.min.css"/>
-      <link as="script" rel="/static/dayjs@1.11.10.min.js"/>
-      <link as="script" rel="/static/dayjs@1.11.10-plugin-advancedFormat.js"/>
-      <link as="script" rel="/static/dayjs@1.11.10-plugin-customParseFormat.js"/>
-      <link as="script" rel="/static/dayjs@1.11.10-plugin-localeData.js"/>
-      <link as="script" rel="/static/dayjs@1.11.10-plugin-quarterOfYear.js"/>
-      <link as="script" rel="/static/dayjs@1.11.10-plugin-weekday.js"/>
-      <link as="script" rel="/static/dayjs@1.11.10-plugin-weekOfYear.js"/>
-      <link as="script" rel="/static/dayjs@1.11.10-plugin-weekYear.js"/>
-      <link as="script" rel="/static/dayjs@1.11.10-locale-zh-cn.js"/>
-      <link as="script" rel="/static/lodash@4.17.21.min.js"/>
-      <link as="script" rel="/static/echarts@5.4.3.min.js"/>
-      <link as="script" rel="/static/axios@0.27.2.min.js"/>
-      <link as="script" rel="/static/easycube-request@0.1.1.iife.min.js"/>
-      <link as="script" rel="/static/popper.js@2.11.8.min.js"/>
-      <link as="script" rel="/static/tippy.js@6.3.7.umd.min.js"/>
-      <link as="script" rel="/static/vue@3.3.4.runtime.global.prod.min.js"/>
-      <link as="script" rel="/static/ant-design-vue@3.2.20-with-locales.min.js"/>
-      <link as="script" rel="/static/request@0.1.0.iife.min.js"/>
-      <link as="script" rel="/static/vue-demi@0.14.6.iife.min.js"/>
-      <link as="script" rel="/static/vueuse_shared@10.4.1.iife.min.js"/>
-      <link as="script" rel="/static/vueuse_core@10.4.1.iife.min.js"/>
-      <link as="script" rel="/static/vue-router@4.1.5.global.min.js"/>
-      <link as="script" rel="/static/pinia@2.1.6.iife.min.js"/>
-      <link as="script" rel="/static/sortable@1.15.0.min.js"/>
-      <link as="script" rel="/static/vuedraggable.umd.min.js"/>
-      <link as="script" rel="/static/easycube-playground@0.1.4.iife.min.js"/>
+const resources = [
+  { name: 'easycube-playground@0.1.4.min.css', type: 'style' },
+  { name: 'dayjs@1.11.10.min.js', type: 'script' },
+  { name: 'dayjs@1.11.10-plugin-advancedFormat.js', type: 'script' },
+  { name: 'dayjs@1.11.10-plugin-customParseFormat.js', type: 'script' },
+  { name: 'dayjs@1.11.10-plugin-localeData.js', type: 'script' },
+  { name: 'dayjs@1.11.10-plugin-quarterOfYear.js', type: 'script' },
+  { name: 'dayjs@1.11.10-plugin-weekday.js', type: 'script' },
+  { name: 'dayjs@1.11.10-plugin-weekOfYear.js', type: 'script' },
+  { name: 'dayjs@1.11.10-plugin-weekYear.js', type: 'script' },
+  { name: 'dayjs@1.11.10-locale-zh-cn.js', type: 'script' },
+  { name: 'lodash@4.17.21.min.js', type: 'script' },
+  { name: 'echarts@5.4.3.min.js', type: 'script' },
+  { name: 'axios@0.27.2.min.js', type: 'script' },
+  { name: 'easycube-request@0.1.1.iife.min.js', type: 'script' },
+  { name: 'popper.js@2.11.8.min.js', type: 'script' },
+  { name: 'tippy.js@6.3.7.umd.min.js', type: 'script' },
+  { name: 'vue@3.3.4.runtime.global.prod.min.js', type: 'script' },
+  { name: 'ant-design-vue@3.2.20-with-locales.min.js', type: 'script' },
+  { name: 'request@0.1.0.iife.min.js', type: 'script' },
+  { name: 'vue-demi@0.14.6.iife.min.js', type: 'script' },
+  { name: 'vueuse_shared@10.4.1.iife.min.js', type: 'script' },
+  { name: 'vueuse_core@10.4.1.iife.min.js', type: 'script' },
+  { name: 'vue-router@4.1.5.global.min.js', type: 'script' },
+  { name: 'pinia@2.1.6.iife.min.js', type: 'script' },
+  { name: 'sortable@1.15.0.min.js', type: 'script' },
+  { name: 'vuedraggable.umd.min.js', type: 'script' },
+  { name: 'easycube-playground@0.1.4.iife.min.js', type: 'script' },
+]
 
-      <link rel="stylesheet" href="/static/easycube-playground@0.1.4.min.css" />
-      <script src="/static/dayjs@1.11.10.min.js" defer></script>
-      <script src="/static/dayjs@1.11.10-plugin-advancedFormat.js" defer></script>
-      <script src="/static/dayjs@1.11.10-plugin-customParseFormat.js" defer></script>
-      <script src="/static/dayjs@1.11.10-plugin-localeData.js" defer></script>
-      <script src="/static/dayjs@1.11.10-plugin-quarterOfYear.js" defer></script>
-      <script src="/static/dayjs@1.11.10-plugin-weekday.js" defer></script>
-      <script src="/static/dayjs@1.11.10-plugin-weekOfYear.js" defer></script>
-      <script src="/static/dayjs@1.11.10-plugin-weekYear.js" defer></script>
-      <script src="/static/dayjs@1.11.10-locale-zh-cn.js" defer></script>
-      <script src="/static/lodash@4.17.21.min.js" defer></script>
-      <script src="/static/echarts@5.4.3.min.js" defer></script>
-      <script src="/static/axios@0.27.2.min.js" defer></script>
-      <script src="/static/easycube-request@0.1.1.iife.min.js" defer></script>
-      <script src="/static/popper.js@2.11.8.min.js" defer></script>
-      <script src="/static/tippy.js@6.3.7.umd.min.js" defer></script>
-      <script src="/static/vue@3.3.4.runtime.global.prod.min.js" defer></script>
-      <script src="/static/ant-design-vue@3.2.20-with-locales.min.js" defer></script>
-      <script src="/static/request@0.1.0.iife.min.js" defer></script>
-      <script src="/static/vue-demi@0.14.6.iife.min.js" defer></script>
-      <script src="/static/vueuse_shared@10.4.1.iife.min.js" defer></script>
-      <script src="/static/vueuse_core@10.4.1.iife.min.js" defer></script>
-      <script src="/static/vue-router@4.1.5.global.min.js" defer></script>
-      <script src="/static/pinia@2.1.6.iife.min.js" defer></script>
-      <script src="/static/sortable@1.15.0.min.js" defer></script>
-      <script src="/static/vuedraggable.umd.min.js" defer></script>
-      <script src="/static/easycube-playground@0.1.4.iife.min.js" defer></script>
-    `
+const preloadResource = resources
+  .map(item => `<link as="${item.type}" ref="preload" href="/static/${item.name}">`)
+  .join('')
+
+const reloadResource = resources
+  .map(item => {
+    if (item.type === 'style') {
+      return `<link rel="stylesheet" href="/static/${item.name}" />`
+    } else {
+      return `<script src="/static/${item.name}" preload></script>`
+    }
+  })
+  .join('')
+
+const rawResource = `
+  ${preloadResource}
+  ${reloadResource}
+`
+
+export default function injectStaticResources(isBuild: boolean): Plugin {
   const plugin: Plugin = {
     name: 'inject-static-resources-plugin',
     transformIndexHtml(html: string) {
       if (isBuild) {
         const newHTML = html.replace(/<head>([\s\S]*?)<\/head>/g, (_, headInnerText) => {
-          return `<head>\n${resources}\n${headInnerText}</head>`
+          return `<head>
+              ${rawResource}
+              ${headInnerText}
+            </head>`
         })
         return newHTML
       } else {
